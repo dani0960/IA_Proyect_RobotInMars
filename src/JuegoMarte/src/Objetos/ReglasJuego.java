618f31faffbdbe9691d5cd594bc01b9ad0d315e0
Eric Ramos <alu0100786330@ull.edu.es>
2015-10-22 15:20:22 +0100

    YEAAAH BABY, este sí

---
+++ src/JuegoMarte/src/Objetos/ReglasJuego.java
@@ -0,0 +1,9 @@
+package Objetos;
+
+// Aquí irán todas las restricciones presentes en el juego.
+public class ReglasJuego {
+
+	void chequear(Tablero tablero, Robot robot) {
+	}
+
+}
---
+++ src/JuegoMarte/src/Reglas/ReglasJuego.java
@@ -0,0 +1,13 @@
+package Reglas;
+
+import GUI.Lienzo;
+import Objetos.Robot;
+
+
+// Aquí irán todas las restricciones presentes en el juego.
+public class ReglasJuego {
+
+	void chequear(Lienzo lienzo, Robot robot) {
+	}
+
+}
