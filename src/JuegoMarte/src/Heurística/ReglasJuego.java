6f9399eb41c22bcf5277919aca3eb5952194191b
Germán Méndez <gcpmendez@gmail.com>
2015-10-22 15:57:03 +0100

    source

---
+++ src/JuegoMarte/src/Heurística/ReglasJuego.java
@@ -0,0 +1,12 @@
+package Heurística;
+
+import Objetos.Personaje;
+import Útiles.MatrizDeJuego;
+
+// Aquí irán todas las restricciones presentes en el juego.
+public class ReglasJuego {
+
+	void chequear(MatrizDeJuego tablero, Personaje robot) {
+	}
+
+}
---
+++ src/JuegoMarte/src/Main/MainGameIA.java
@@ -0,0 +1,38 @@
+package Main;
+
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+import GUI.Applet;
+
+public class MainGameIA {
+
+	public static void main(String[] args) {
+		// Look & Feel del SO.
+		try
+		{
+		    JFrame.setDefaultLookAndFeelDecorated(true);
+		    JDialog.setDefaultLookAndFeelDecorated(true);
+		    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}
+		catch (Exception e)
+		{
+		    e.printStackTrace();
+		}
+
+		JFrame frame = new JFrame();
+		Applet applet = new Applet();
+		applet.init();
+
+		frame.add(applet);
+		frame.setTitle("Robot en Marte");
+		frame.setSize(700, 600);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+//		frame.setExtendedState(6); // Maximizar ventana
+	}
+
+}
